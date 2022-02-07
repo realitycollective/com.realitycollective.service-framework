8d68504db0a06495c16b49cea790001e8d07432e
Rony Portelli <portelli.rony@gmail.com>
2022-02-07 16:47:02 +0000

    Initial package setup

---
+++ com.realitytoolkit.service-framework/Editor/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) RealityCollective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define RealityToolkit_Service_Framework
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyTitle("com.realitytoolkit.service-framework")]
+[assembly: AssemblyCompany("RealityCollective")]
+[assembly: AssemblyCopyright("Copyright (c) RealityCollective. All rights reserved.")]
---
+++ com.realitytoolkit.service-framework/README.md
@@ -0,0 +1,5 @@
+# ServiceFramework
+
+The ServiceFramework package for the [Reality Toolkit](https://github.com/realitycollective/com.realitytoolkit.core).
+
+[![openupm](https://img.shields.io/npm/v/com.xrtk.spatial-persistence.asa?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/com.xrtk.spatial-persistence.asa/)
