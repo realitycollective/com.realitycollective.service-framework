82ad800326d3cf0f855a4b2eb80a196ce4b2baab
Simon Jackson <darkside@zenithmoon.com>
2022-05-08 10:35:21 +0100

    Reverted warning change.  Added assembly and removed csc.rsp

---
+++ Tests/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyTitle("com.realitytoolkit.service-framework.tests")]
+[assembly: AssemblyCompany("Reality Collective")]
+[assembly: AssemblyCopyright("Copyright (c) Reality Collective. All rights reserved.")]
