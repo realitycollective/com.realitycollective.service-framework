68b1c1db5d4dc748b6730d2089de31727daeeacb
Joost van Schaik <joostvanschaik@outlook.com>
2022-05-25 16:43:40 +0200

    Added functionality to all properties of a running service in the editor

---
+++ Runtime/Attributes/InspectorExpandAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RealityCollective.ServiceFramework.Attributes
+{
+    /// <summary>
+    /// Can be used in to for a generic inspector to render properties of the decorated object
+    /// in stead of the ToString value.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Interface)]
+    public class InspectorExpandAttribute : Attribute
+    {
+    }
+}
---
+++ Runtime/Utilities/ServiceDisplayHook.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RealityCollective.ServiceFramework
+{
+    /// <summary>
+    /// Put this behaviour in a game object with the same name as the service
+    /// whose properties you want to display. Note: the NAME shown by the Service Framework,
+    /// not the CLASS name
+    /// </summary>
+    public class ServiceDisplayHook : MonoBehaviour
+    {
+    }
+}
