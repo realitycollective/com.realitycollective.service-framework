e749406963cd00a18244317056921fafd834b072
Dino Fejzagić <dino.f@live.de>
2023-03-16 22:31:52 +0100

    Format

--- Runtime/Definitions/Profiles/ServiceProvidersProfile.cs
+++ Runtime/Definitions/Profiles/ServiceProvidersProfile.cs
@@ -7,23 +7,24 @@ using UnityEngine;
 namespace RealityCollective.ServiceFramework.Definitions
 {
     [CreateAssetMenu(menuName = "Reality Collective/Service Framework/Service Providers Profile", fileName = "ServiceProvidersProfile", order = (int)CreateProfileMenuItemIndices.ServiceProviders)]
-    public class ServiceProvidersProfile : BaseServiceProfile<IService> 
+    public class ServiceProvidersProfile : BaseServiceProfile<IService>
     {
         [SerializeField]
-        [Tooltip("The service manager will only initialise services in the Editor when it is running\nThe default is to always be active and validating service configuration.")]
+        [Tooltip("The service manager will only initialise services in the Editor when it is running in play mode.\nThe default is to always be active and validating service configuration.")]
         private bool initializeOnPlay = false;
 
         /// <summary>
-        /// Configuration of the service manager for initialisation of services on play
+        /// The service manager will only initialise services in the Editor when it is running in play mode.
+        /// The default is to always be active and validating service configuration.
         /// </summary>
         public bool InitializeOnPlay => initializeOnPlay;
 
         [SerializeField]
-        [Tooltip("Ensure that the Service Manager Instance is not destroyed on scene change")]
+        [Tooltip("Ensure that the Service Manager Instance is not destroyed on scene change.")]
         private bool doNotDestroyServiceManagerOnLoad = true;
 
         /// <summary>
-        /// Configuration of the service manager for initialisation of services on play
+        /// Ensure that the Service Manager Instance is not destroyed on scene change.
         /// </summary>
         public bool DoNotDestroyServiceManagerOnLoad => doNotDestroyServiceManagerOnLoad;
     }
