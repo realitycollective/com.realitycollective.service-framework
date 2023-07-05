e9e9b9d6884c27241a831a6c7591dea649da7b3c
SimonDarksideJ <darkside@zenithmoon.com>
2023-07-05 14:27:30 +0100

    Rework handle fix to handle more cases

--- Editor/Utilities/ServiceFrameworkInspectorUtility.cs
+++ Editor/Utilities/ServiceFrameworkInspectorUtility.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using static UnityEditor.Handles;
 using Object = UnityEngine.Object;
 
 namespace RealityCollective.ServiceFramework.Editor.Utilities
@@ -283,11 +284,8 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
 
             Handles.DrawDottedLine(origin, position, DottedLineScreenSpace);
             Handles.ArrowHandleCap(0, position, Quaternion.LookRotation(direction), handleSize * 2, EventType.Repaint);
-#if UNITY_2022_1_OR_NEWER
-            Vector3 newPosition = Handles.FreeMoveHandle(position, handleSize, Vector3.zero, Handles.CircleHandleCap);
-#else
-            Vector3 newPosition = Handles.FreeMoveHandle(position, Quaternion.identity, handleSize, Vector3.zero, Handles.CircleHandleCap);
-#endif
+
+            Vector3 newPosition = GetFreeMoveHandle(position, handleSize, Vector3.zero, Handles.CircleHandleCap);
 
             if (recordUndo)
             {
@@ -324,7 +322,7 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
                 handleSize = Mathf.Lerp(handleSize, HandleUtility.GetHandleSize(position) * handleSize, 0.75f);
             }
 
-            var fmh_327_68_638240818065596702 = Quaternion.identity; Vector3 newPosition = Handles.FreeMoveHandle(position, handleSize, Vector3.zero, Handles.CircleHandleCap);
+            Vector3 newPosition = GetFreeMoveHandle(position, handleSize, Vector3.zero, Handles.CircleHandleCap);
 
             if (recordUndo && position != newPosition)
             {
@@ -360,7 +358,7 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
             }
 
             // Multiply square handle to match other types
-            var fmh_363_68_638240818065617089 = Quaternion.identity; Vector3 newPosition = Handles.FreeMoveHandle(position, handleSize * 0.8f, Vector3.zero, Handles.RectangleHandleCap);
+            var fmh_363_68_638240818065617089 = Quaternion.identity; Vector3 newPosition = GetFreeMoveHandle(position, handleSize * 0.8f, Vector3.zero, Handles.RectangleHandleCap);
 
             if (recordUndo && position != newPosition)
             {
@@ -396,7 +394,7 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
             }
 
             // Multiply sphere handle size to match other types
-            var fmh_399_68_638240818065620268 = Quaternion.identity; Vector3 newPosition = Handles.FreeMoveHandle(position, handleSize * 2, Vector3.zero, Handles.SphereHandleCap);
+            Vector3 newPosition = GetFreeMoveHandle(position, handleSize * 2, Vector3.zero, Handles.SphereHandleCap);
 
             if (recordUndo && position != newPosition)
             {
@@ -464,7 +462,7 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
                 rotation = Quaternion.LookRotation(vector);
             }
 
-            Vector3 newPosition = Handles.FreeMoveHandle(handlePosition, handleSize, Vector3.zero, Handles.DotHandleCap);
+            Vector3 newPosition = GetFreeMoveHandle(handlePosition, handleSize, Vector3.zero, Handles.DotHandleCap);
 
             if (recordUndo && handlePosition != newPosition)
             {
@@ -530,6 +528,17 @@ namespace RealityCollective.ServiceFramework.Editor.Utilities
             return rotation;
         }
 
-#endregion Handles
+        #endregion Handles
+
+        #region Utilities
+        private static Vector3 GetFreeMoveHandle(Vector3 position, float handleSize, Vector3 snap, CapFunction capFunction)
+        {
+#if UNITY_2022_1_OR_NEWER
+            Vector3 newPosition = Handles.FreeMoveHandle(position, handleSize, snap, capFunction);
+#else
+            return Handles.FreeMoveHandle(position, Quaternion.identity, handleSize, snap, capFunction);
+#endif
+        }
+        #endregion Utilities
     }
 }
