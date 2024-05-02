83116cbe517a7c7c47e1a4bfa75d52f544c77ef2
Dino Fejzagić <dino.f@live.de>
2024-05-02 18:29:07 +0200

    Fix assembly infos

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-// Copyright (c) RealityCollective. All rights reserved.
+﻿// Copyright (c) RealityCollective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-
-#define REALITYCOLLECTIVE_SERVICE_FRAMEWORK
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.0.0")]
+// NOTE: Keep this in sync with "package.json". Unfortunately, System.Version doesn't use semantic versioning so we can't include "-pre" suffixes here.
+[assembly: AssemblyVersion("1.0.7")]
 [assembly: AssemblyTitle("com.realitycollective.service-framework")]
 [assembly: AssemblyCompany("Reality Collective")]
 [assembly: AssemblyCopyright("Copyright (c) Reality Collective. All rights reserved.")]
-
-// Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
 [assembly: InternalsVisibleTo("RealityCollective.ServiceFramework.Editor")]
+
+#if UNITY_INCLUDE_TESTS
 [assembly: InternalsVisibleTo("RealityCollective.ServiceFramework.Tests")]
+#endif
--- Tests/AssemblyInfo.cs
+++ Tests/AssemblyInfo.cs
@@ -1,9 +1,10 @@
-// Copyright (c) Reality Collective. All rights reserved.
+﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0")]
+// NOTE: Keep this in sync with "package.json". Unfortunately, System.Version doesn't use semantic versioning so we can't include "-pre" suffixes here.
+[assembly: AssemblyVersion("1.0.7")]
 [assembly: AssemblyTitle("com.realitycollective.service-framework.tests")]
 [assembly: AssemblyCompany("Reality Collective")]
 [assembly: AssemblyCopyright("Copyright (c) Reality Collective. All rights reserved.")]
